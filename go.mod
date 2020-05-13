2723c76ce67f3160d3a8e9e3b66c57f4fbbf3056
m-ssilva <bleed-it-out@live.com>
2020-05-13 15:28:02 -0300

    initial structure

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/m-ssilva/api.go.gingonic
+
+go 1.14
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ services/register.service.go
@@ -0,0 +1,7 @@
+package services
+
+import "github.com/gin-gonic/gin"
+
+// RegisterService is service layer of register endpoint
+func RegisterService(c *gin.Context) {
+}
